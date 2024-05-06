b0575298e2ba2427450e441cc3721bc018f9db0b
MikhaiLS9 <famm93@mail.ru>
2024-05-06 12:04:27 +0300

    modal-page_close-button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
 import { Link1Component } from './pages/link1/link1.component';
 import { Link2Component } from './pages/link2/link2.component';
 import { Link3Component } from './pages/link3/link3.component';
+import { ModalPageComponent } from './modal-page/modal-page.component';
 
 @NgModule({
-  declarations: [AppComponent, Link1Component, Link2Component, Link3Component],
+  declarations: [AppComponent, Link1Component, Link2Component, Link3Component,ModalPageComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- src/app/modal-page/modal-page.component.ts
+++ src/app/modal-page/modal-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal-page',
   templateUrl: './modal-page.component.html',
   styleUrls: ['./modal-page.component.scss'],
 })
-export class ModalPageComponent  {
-  constructor() { }
+export class ModalPageComponent {
+  constructor(private modalCtrl: ModalController) {}
+
+  async closeModalpage() {
+    await this.modalCtrl.dismiss();
+  }
 }
